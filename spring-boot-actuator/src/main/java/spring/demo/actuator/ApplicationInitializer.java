186701220fa4ab18d722c449311ffe7ceab42f6d
wangoo <wangoo@soundbus.cn>
2017-05-24 15:22:16 +0800

    add actuator demo

---
+++ spring-boot-actuator/src/main/java/spring/demo/actuator/ApplicationInitializer.java
@@ -0,0 +1,17 @@
+/**
+ * @created 2014年11月2日 上午9:32:06
+ */
+package spring.demo.actuator;
+
+/**
+ * @author gelnyang
+ * @created 2014年11月2日 上午9:32:06
+ * @version 1.0
+ */
+@Spr
+public class ApplicationInitializer {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(ApplicationInitializer.class, args);
+    }
+}
